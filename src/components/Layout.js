9517cf8710a002f1d097b481ea29b4635f5005df
deji-404 <ayodejiosayemi928@gmail.com>
2023-11-11 18:45:40 +0100

    first commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Layout = ({ children, className="" }) => {
     return (
-        <div className={`w-full h-full inline-block bg-light z-0 p-32 ${className}`}>
+        <div className={`w-full h-full inline-block bg-light dark:bg-dark z-0 p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}>
             {children}
         </div>
     )
